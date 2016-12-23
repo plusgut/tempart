63ce617df3dc0ac74ce20549d83fb25951b525ce
Carlo Jeske <jeske@sc-networks.com>
2016-12-23 12:58:50 +0100

    [karma] coding guidelines

--- karma.conf.js
+++ karma.conf.js
@@ -7,11 +7,9 @@ module.exports = function (config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -19,46 +17,37 @@ module.exports = function (config) {
       'test/**/*Test.js',
     ],
 
-
     // list of files to exclude
     exclude: [
     ],
 
-
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'dist/tempart.js': ['coverage']
+      'dist/tempart.js': ['coverage'],
     },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'coverage'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -69,8 +58,8 @@ module.exports = function (config) {
     concurrency: Infinity,
 
     coverageReporter: {
-      type : 'lcov',
-      dir : 'test/coverage/'
-    }
+      type: 'lcov',
+      dir: 'test/coverage/',
+    },
   });
 };
